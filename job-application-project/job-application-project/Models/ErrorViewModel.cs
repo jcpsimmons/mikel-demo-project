4d99a868c9f21fdacd7b03177f2edebc24879e08
simsies <jcpsimmons@gmail.com>
2019-05-02 20:14:43 -0700

    view running

---
+++ job-application-project/job-application-project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace job_application_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
